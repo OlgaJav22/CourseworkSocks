23dbd5f8efe941fba1308cf4a8c0df00ec3a5091
OlgaJav22 <olga.ko4erga@yandex.ru>
2023-02-03 20:41:46 +0300

    начало

---
+++ src/main/java/com/example/courseworksocks/CourseworkSocksApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseworksocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkSocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseworkSocksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/courseworksocks/CourseworkSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseworksocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkSocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
